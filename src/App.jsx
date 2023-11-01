b5aab61a1ebc0d87c4a47a6320e6971537479199
sidad123 <sidevmir77@gmail.com>
2023-11-02 00:06:42 +0200

    changed headingText

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     <div>
       <p>Hello</p>
 
-      <MovieList movies={movies} headingText="Movie Listt"/>
+      <MovieList movies={movies} headingText="Movie List Here"/>
     </div>
   )
 }
