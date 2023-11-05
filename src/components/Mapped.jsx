27cefd932beb9a5e812e4853ea71d142ff13de76
sidad123 <sidevmir77@gmail.com>
2023-11-05 02:39:54 +0200

    added keys and mapped

--- src/components/Mapped.jsx
+++ src/components/Mapped.jsx
@@ -1,10 +1,24 @@
 export default function MappedMovies(props) {
 
-    let items = props.items.map((item) =><li>{item.title} - {item.description}</li>)
-
+    let items = props.items.map((item) =><li key={`${item.title}-${item.description}`}> {item.title} - {item.description}</li>)
+    console.log(items);
     return (
         <ul>
             {items}
         </ul>
     );
 };
+
+
+
+//This is before adding key, trqbva key za da identificira elementite
+// export default function MappedMovies(props) {
+
+//     let items = props.items.map((item) =><li>{item.title} - {item.description}</li>)
+
+//     return (
+//         <ul>
+//             {items}
+//         </ul>
+//     );
+// };
